8193e60b0baa86ff74e43b1cf03711094cb35af3
birkin <birkin.diana@gmail.com>
2015-08-17 11:41:40 -0400

    updates logger.

--- utils/log_helper.py
+++ utils/log_helper.py
@@ -13,14 +13,13 @@ import logging, os
 def setup_logger():
     """ Returns a logger to write to a file.
         Called by papiweb_app.py """
-    LOG_DIR = unicode( os.environ.get('papiweb__LOG_DIR') )
+    LOG_PATH = unicode( os.environ.get('papiweb__LOG_PATH') )
     LOG_LEVEL = unicode( os.environ.get('papiweb__LOG_LEVEL') )
-    filename = '%s/papiweb.log' % LOG_DIR
     formatter = logging.Formatter( '[%(asctime)s] %(levelname)s | %(module)s->%(funcName)s() | ln %(lineno)d | %(message)s' )
     logger = logging.getLogger( __name__ )
     level_dict = { 'debug': logging.DEBUG, 'info':logging.INFO }
     logger.setLevel( level_dict[LOG_LEVEL] )
-    file_handler = logging.FileHandler( filename )
+    file_handler = logging.FileHandler( LOG_PATH )
     file_handler.setFormatter( formatter )
     logger.addHandler( file_handler )
     return logger
