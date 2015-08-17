f45b41cb30f6065c69c99a5f39f05b1b170c4734
birkin <birkin.diana@gmail.com>
2015-08-17 10:49:23 -0400

    renames wsgi file.

--- config/passenger_wsgi.py
+++ config/passenger_wsgi.py
@@ -5,10 +5,10 @@ from __future__ import unicode_literals
 """ Prepares application environment.
     Variables assume project setup like:
     some_enclosing_directory/
-        bdpyweb_code/
+        papiweb_code/
             config/
-            bdpyweb_app.py
-        env_bdpyweb/
+            papiweb_app.py
+        env_papiweb/
      """
 
 import os, pprint, sys
@@ -18,10 +18,10 @@ import os, pprint, sys
 current_directory = os.path.dirname( os.path.abspath(__file__) )
 
 ## vars
-ACTIVATE_FILE = os.path.abspath( '%s/../../env_bdpyweb/bin/activate_this.py' % current_directory )
-PROJECT_DIR = os.path.abspath( '%s/../../bdpyweb_code' % current_directory )
+ACTIVATE_FILE = os.path.abspath( '%s/../../env_papiweb/bin/activate_this.py' % current_directory )
+PROJECT_DIR = os.path.abspath( '%s/../../papiweb_code' % current_directory )
 PROJECT_ENCLOSING_DIR = os.path.abspath( '%s/../..' % current_directory )
-SITE_PACKAGES_DIR = os.path.abspath( '%s/../../env_bdpyweb/lib/python2.7/site-packages' % current_directory )
+SITE_PACKAGES_DIR = os.path.abspath( '%s/../../env_papiweb/lib/python2.7/site-packages' % current_directory )
 
 ## load virtual env
 execfile( ACTIVATE_FILE, dict(__file__=ACTIVATE_FILE) )
@@ -32,10 +32,10 @@ for entry in [PROJECT_DIR, PROJECT_ENCLOSING_DIR, SITE_PACKAGES_DIR]:
    sys.path.append( entry )
 
 ## load up env vars
-SETTINGS_FILE = os.environ['bdpyweb__SETTINGS_PATH']  # set in activate_this.py, and activated above
+SETTINGS_FILE = os.environ['papiweb__SETTINGS_PATH']  # set in activate_this.py, and activated above
 import shellvars
 var_dct = shellvars.get_vars( SETTINGS_FILE )
 for ( key, val ) in var_dct.items():
     os.environ[key] = val
 
-from bdpyweb_code.bdpyweb_app import app as application
+from papiweb_code.papiweb_app import app as application
