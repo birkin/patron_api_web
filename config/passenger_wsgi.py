f475388cb558928db346a6af09db4ff6cf41d33a
birkin <birkin.diana@gmail.com>
2019-08-19 13:19:35 -0400

    simplifies config file.

--- config/passenger_wsgi.py
+++ config/passenger_wsgi.py
@@ -12,25 +12,30 @@
 import logging, os, pprint, sys
 
 
+# ## become self-aware, padawan
+# current_directory = os.path.dirname( os.path.abspath(__file__) )
+
+# ## vars
+# # ACTIVATE_FILE = os.path.abspath( '%s/../../env3_papiweb/bin/activate_this.py' % current_directory )
+# PROJECT_DIR = os.path.abspath( '%s/../../papiweb_code' % current_directory )
+# PROJECT_ENCLOSING_DIR = os.path.abspath( '%s/../..' % current_directory )
+# SITE_PACKAGES_DIR = os.path.abspath( '%s/../../env3_papiweb/lib/python3.6/site-packages' % current_directory )
+
+# ## sys.path additions
+# for entry in [PROJECT_DIR, PROJECT_ENCLOSING_DIR, SITE_PACKAGES_DIR]:
+#  if entry not in sys.path:
+#    sys.path.append( entry )
+
 ## become self-aware, padawan
-current_directory = os.path.dirname( os.path.abspath(__file__) )
-
-## vars
-# ACTIVATE_FILE = os.path.abspath( '%s/../../env3_papiweb/bin/activate_this.py' % current_directory )
-PROJECT_DIR = os.path.abspath( '%s/../../papiweb_code' % current_directory )
-PROJECT_ENCLOSING_DIR = os.path.abspath( '%s/../..' % current_directory )
-SITE_PACKAGES_DIR = os.path.abspath( '%s/../../env3_papiweb/lib/python3.6/site-packages' % current_directory )
-
-## load virtual env
-# execfile( ACTIVATE_FILE, dict(__file__=ACTIVATE_FILE) )
-
-## sys.path additions
-for entry in [PROJECT_DIR, PROJECT_ENCLOSING_DIR, SITE_PACKAGES_DIR]:
+CONFIG_DIR = os.path.dirname( os.path.abspath(__file__) )
+PROJECT_DIR = os.path.dirname( CONFIG_DIR )  # papiweb_code
+PROJECT_ENCLOSING_DIR = os.path.dirname( PROJECT_DIR )
+for entry in [PROJECT_DIR, PROJECT_ENCLOSING_DIR]:
  if entry not in sys.path:
    sys.path.append( entry )
 
 ## load up env vars
-SETTINGS_FILE = os.environ['papiweb__SETTINGS_PATH']  # set in activate_this.py, and activated above
+SETTINGS_FILE = os.environ['papiweb__SETTINGS_PATH']
 import shellvars
 var_dct = shellvars.get_vars( SETTINGS_FILE )
 for ( key, val ) in var_dct.items():
